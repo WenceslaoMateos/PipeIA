71d656bd67675c2e002d4a7cee213b42ba47a8e7
wenceslao <wenceslao@Jade-Notebook-Ubuntu>
2018-10-15 20:05:14 -0300

    Base de Pipe en prolog para revisar

---
+++ pipe.pl
@@ -0,0 +1,13 @@
+piezas_disponibles().
+
+origen(X, Y).
+
+destino(X, Y).
+
+piezas().
+
+%trabajando con asserts no precisamos de una matriz.
+
+%al momento en que origen y destino es el mismo, quiere decir que llegaste.
+
+%el resultado es un listado de estructuras conformadas por piezas y posición.
